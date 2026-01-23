84c566a92e1fba2aa3d043c888da07dad5a5f0f6
kasunSMTK <kasun@softmastergroup.com>
2026-01-23 11:29:48 +0530

    section 4

--- API/Controllers/ActivityController.cs
+++ API/Controllers/ActivityController.cs
@@ -1,12 +1,15 @@
 using System;
+using Application.Activities.Commands;
+using Application.Activities.Queries;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
+// using Microsoft.EntityFrameworkCore;
+// using Persistence;
 
 namespace API.Controllers;
-
-public class ActivityController(AppDbContext context) : BaseApiController
+//AppDbContext context, without mdiator need pass to constructor
+public class ActivityController : BaseApiController
 {
     // private readonly AppDbContext context;
     // public ActivityController(AppDbContext context)
@@ -16,17 +19,43 @@ public class ActivityController(AppDbContext context) : BaseApiController
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivies()
     {
-        
-        return await context.Activities.ToListAsync();
+
+        //  return await context.Activities.ToListAsync();
+        return await Mediator.Send(new GetActivityList.Query());
     }
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetails(string id)
     {
-        var activity= await context.Activities.FindAsync(id);
+        // var activity = await context.Activities.FindAsync(id);
 
-        if (activity == null) return NotFound();
+        // if (activity == null) return NotFound();
 
-        return activity;
+        // return activity;
 
+        return await Mediator.Send(new GetActivityDetails.Query{Id = id});
+
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<string>> CreateActivity(Activity activity)
+    {
+        return await Mediator.Send(new CreateActivity.Command{Activity = activity});
+        
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> EditActivity(Activity activity)
+    {
+        await Mediator.Send(new EditActivity.Command{Activity = activity});
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteActivity(string id)
+    {
+        await Mediator.Send(new DeleteActivity.Command{id = id});
+        
+        return Ok();
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Application.Activities.Queries;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -6,19 +8,24 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>(opt =>{
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+builder.Services.AddMediatR(x =>
+x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
-.WithOrigins("http://localhost:3000","https://localhost:3000"));
+.WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
 app.MapControllers();
 
-using var scope = app. Services.CreateScope();
+using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 try
 {
@@ -28,8 +35,8 @@ try
 }
 catch (Exception ex)
 {
-    var logger =services.GetRequiredService<ILogger<Program>>();
+    var logger = services.GetRequiredService<ILogger<Program>>();
     logger.LogError(ex, "An Error occurred durring migration.");
-}    
+}
 
 app.Run();
